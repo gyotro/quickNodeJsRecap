c470efa7bd703261b4b0d8d4cf0a73601e2eaf34
giovanni <giovanni.dintrono@gmail.com>
2024-01-16 16:13:58 +0000

    proof with OS

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { saludar, saludaTodoMundo } = require("./saludo");
+const os = require("os");
 
 //const saludo = require("./saludo");
 
 console.log(saludar("Juan"));
 saludaTodoMundo();
+
+console.log(os.platform(), os.homedir(), os.userInfo());
