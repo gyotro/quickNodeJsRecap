b29b6a267cf17e7891b4411be898d8618029aa38
gyotro <giovanni.dintrono@gmail.com>
2024-01-16 16:25:17 +0000

    test with timeout

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { setTimeout } = require("timers");
 const { saludar, saludaTodoMundo } = require("./saludo");
 const os = require("os");
 
@@ -7,3 +8,5 @@ console.log(saludar("Juan"));
 saludaTodoMundo();
 
 console.log(os.platform(), os.homedir(), os.userInfo());
+
+setTimeout(saludar, 2000, "JuanRetraso");
--- saludo.js
+++ saludo.js
@@ -1,4 +1,5 @@
 function saludar(nombre) {
+  console.log(`Hola ${nombre}!`);
   return `Hola ${nombre}!`;
 }
 
