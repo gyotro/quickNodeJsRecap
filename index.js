e7ee51189d2296cac492a983b52c58e1168b0326
gyotro <giovanni.dintrono@gmail.com>
2024-01-19 16:40:20 +0100

    adding router

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const { dataTest } = require("./data")
 // creating express object
 const app = express()
 
+// creating router object
+const routerCities = express.Router()
+// ligamos el router con el path
+app.use('/api/cities', routerCities)
+
 // Routing
 app.get( '/', ( req, res ) => {
     res.send( 'Servidor Arrancado!' )
@@ -12,19 +17,19 @@ app.get( '/', ( req, res ) => {
 // con :name capturamos directamente la parte del URL despues de /cities/ y la colocamos en la variable name (el nombre podía ser cualquiera) 
 // se pueden utilizar mas parametros de busqueda /:nameCity/:country 
 // para tomar los query parameters la sintaxis es: req.query.<nombre_parametro>
-app.get( '/api/cities/:nameCity', ( req, res ) => {
+routerCities.get( '/:nameCity', ( req, res ) => {
     const nameCity = req.params.nameCity
     const resultados = dataTest.cities.filter( (city) => city.name === nameCity )
 
     if(resultados.length == 0) {
-        res.status(404).send(JSON.stringify({"message":'No city found!!'}))
+       return  res.status(404).send(JSON.stringify({"message":'No city found!!'}))
     }
 
-    res.status(200).send(JSON.stringify(resultados))
+    return res.status(200).send(JSON.stringify(resultados))
 
 } )
 
-app.get( '/api/cities', ( req, res ) => {
+routerCities.get( '/', ( req, res ) => {
     res.send( dataTest.cities )
 } )
 
