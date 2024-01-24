9ab2cb1efeac056bec7910cf890cae835dad4de2
gyotro <giovanni.dintrono@gmail.com>
2024-01-24 15:13:49 +0100

    starting MERN project

--- build/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// npm i typescript 
-// npx tsc --init
-// npx tsc -w
-// node (ruta del fichero nodeJs generado)
-var saludo = "Hola TypeScript!!";
-console.log(saludo);
--- test.ts
+++
@@ -1,8 +0,0 @@
-// npm i typescript 
-// npx tsc --init
-// npx tsc -w
-// node (ruta del fichero nodeJs generado)
-
-var saludo: String  = "Hola TypeScript!!";
-
-console.log(saludo);
